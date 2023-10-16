8e558edd769a8a1036d38adcaf54962d60134110
Gomez-Oscar <alexgomez212121@gmail.com>
2023-10-15 19:56:47 -0500

    creando solo interfaz del login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main className='login__container'>
+      <h1 className='login_title'>Good to see you again</h1>
+      <form className='login_form'>
+        <label>Your email</label>
+        <input type='email' placeholder='e.g. elon@tesla.com' />
+        <label>Your password</label>
+        <input type='password' placeholder='e.g. xxxxxxxxxx' />
+        <button type='submit'>Sign In</button>
+      </form>
+    </main>
+  );
 };
 
 export default Login;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
 import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path='/' element={<Home />} />
+          {/* <Route path='/' element={<Home />} /> */}
+          <Route path='/' element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
