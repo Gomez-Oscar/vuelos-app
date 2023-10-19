8b5b2a5bf8da67c46c37105577362d253197457c
Gomez-Oscar <alexgomez212121@gmail.com>
2023-10-19 14:42:47 -0500

    agregando funcionalidad a los botons de ida e ida y vuelta

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,31 +2,37 @@ import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './home.scss';
-import flechas from '../../assets/icons/flechas-derecha-izquierda.png';
+import flechasNegras from '../../assets/icons/flechas-negras.svg';
+import flechasVerdes from '../../assets/icons/flechas-verdes.svg';
 import flechaDerechaVerde from '../../assets/icons/flecha-derecha-verde.svg';
 import flechaDerechaNegra from '../../assets/icons/flecha-derecha-negra.svg';
 import avatar from '../../assets/icons/avatar.png';
 import tipoHaciento from '../../assets/icons/tipo-de-hacientos-icon.png';
-import flechaDespliegue from '../../assets/icons/flecha-despliegue.png';
 import buttonLupa from '../../assets/icons/Button-lupa.png';
 import dateIcon from '../../assets/icons/date-icon.png';
 import mapAvion from '../../assets/img/map-avion.png';
 
 const Home = () => {
   const [startDate, setStartDate] = useState(new Date());
-
   const [returnedDate, setReturnedDate] = useState(new Date());
 
   const [isOpen, setIsOpen] = useState(false);
   const [Open, setOpen] = useState(false);
 
-  const handleClick = e => {
-    e.preventDefault();
+  const [hideReturnDatebutton, setHideReturnDatebutton] = useState(false);
+
+  const handleOneWayClick = () => {
+    setHideReturnDatebutton(true);
+  };
+  const handleRoundTripClick = () => {
+    setHideReturnDatebutton(false);
+  };
+
+  const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleClickReturn = e => {
-    e.preventDefault();
+  const handleClickReturn = () => {
     setOpen(!Open);
   };
 
@@ -35,12 +41,36 @@ const Home = () => {
       <main className='container__principal'>
         <h1 className='find__your__flight'>Find your flight</h1>
         <section className='container__button'>
-          <button className='round__trip'>
-            <img src={flechas} alt='flechas' />
+          <button
+            className={
+              !hideReturnDatebutton ? 'round__trip active' : 'round__trip'
+            }
+            onClick={handleRoundTripClick}
+          >
+            {hideReturnDatebutton ? (
+              <img src={flechasNegras} alt='flechas' />
+            ) : (
+              <img src={flechasVerdes} alt='flechas' />
+            )}
             Round trip
           </button>
-          <button className='one__way'>
-            <img src={flechaDerechaNegra} alt='flecha__derecha' />
+          <button
+            className={hideReturnDatebutton ? 'one__way active' : 'one__way'}
+            onClick={handleOneWayClick}
+          >
+            {hideReturnDatebutton ? (
+              <img
+                src={flechaDerechaVerde}
+                alt='flecha__derecha'
+                className='flecha__derecha'
+              />
+            ) : (
+              <img
+                src={flechaDerechaNegra}
+                alt='flecha__derecha'
+                className='flecha__derecha'
+              />
+            )}
             One way
           </button>
           <button className='cantidad__personas'>
@@ -65,7 +95,7 @@ const Home = () => {
               placeholder='Houston (HOU)'
             />
           </div>
-          <img className='lugar__destino__flechas' src={flechas} alt='' />
+          <img className='lugar__destino__flechas' src={flechasNegras} alt='' />
           <div className='lugar__destino__where'>
             <h1>To</h1>
             <input
@@ -84,29 +114,35 @@ const Home = () => {
             Departure date
           </button>
           {isOpen && (
-            <DatePicker
-              selected={startDate}
-              onChange={date => setStartDate(date)}
-              open={isOpen}
-              inline
-            />
+            <div className='datepicker__container'>
+              <DatePicker
+                className='DatePicker'
+                selected={startDate}
+                onChange={date => setStartDate(date)}
+                open={isOpen}
+                // inline
+              />
+            </div>
           )}
-          <button
-            className='calendar__Returned__date'
-            onClick={handleClickReturn}
-          >
-            <img className='date__icon' src={dateIcon} alt='' />
-            Returned date
-          </button>
-          {Open && (
-            <DatePicker
-              className='container__calendar'
-              selected={returnedDate}
-              onChange={date => setReturnedDate(date)}
-              open={Open}
-              inline
-            />
+          {!hideReturnDatebutton && (
+            <button
+              className='calendar__Returned__date'
+              onClick={handleClickReturn}
+            >
+              <img className='date__icon' src={dateIcon} alt='' />
+              Returned date
+            </button>
           )}
+          <div className='datepicker__container'>
+            {Open && (
+              <DatePicker
+                selected={returnedDate}
+                onChange={date => setReturnedDate(date)}
+                open={Open}
+                // inline
+              />
+            )}
+          </div>
         </section>
         <aside className='mapAvion'>
           <img className='img__map' src={mapAvion} alt='' />
