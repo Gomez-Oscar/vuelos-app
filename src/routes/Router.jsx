4ca310e8ac84e6c7699766db4e11bfe29d64083c
Gomez-Oscar <alexgomez212121@gmail.com>
2023-10-23 23:41:57 -0500

    haciendo otra prueba 1

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -7,7 +7,7 @@ import Login from '../pages/login/Login';
 import Register from '../pages/register/Register';
 import PrivateRoutes from './PrivateRoutes';
 import PublicRoutes from './PublicRoutes';
-import Ticket from '../pages/tickets/ticket';
+// import Ticket from '../pages/tickets/ticket';
 import Detalles from '../pages/detalles-vuelo/detalles.jsx';
 import Formulario from '../components/booking/Booking';
 
@@ -34,7 +34,7 @@ const Router = () => {
                 <Route path='/home' element={<Home />} />
                 <Route path='/detalles' element={<Detalles />} />
                 <Route path='/booking' element={<Formulario />} />
-                <Route path='/tickets' element={<Ticket />} />
+                {/* <Route path='/tickets' element={<Ticket />} /> */}
               </Route>
               <Route element={<PublicRoutes isAuthenticate={isUserLogged} />}>
                 <Route path='/login' element={<Login />} />
